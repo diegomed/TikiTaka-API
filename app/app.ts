42a920e5b073b5046f84b729f282772adf2cd04d
diegomed <diegomedina92@gmail.com>
2019-05-14 23:06:46 -0300

    test commit

--- app/app.ts
+++ app/app.ts
@@ -5,7 +5,7 @@ import express = require('express');
 const app: express.Application = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Worldz!');
 });
 
 app.listen(3000, function () {
