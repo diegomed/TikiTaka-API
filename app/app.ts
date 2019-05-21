f9fcc20be2c6309e81cf5ae7749b188eafcd201d
diegomed <diegomedina92@gmail.com>
2019-05-20 22:11:20 -0300

    Adding auto detectable port for cloud

--- app/app.ts
+++ app/app.ts
@@ -4,10 +4,12 @@ import express = require('express');
 // Create a new express application instance
 const app: express.Application = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
-  res.send('Hello Worldz!');
+  res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port ' + port + '!');
 });
--- build/app.js
+++ build/app.js
@@ -4,10 +4,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 // Create a new express application instance
 var app = express();
+var port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port ' + port + '!');
 });
 //# sourceMappingURL=app.js.map
